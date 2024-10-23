2a16d6911784e612b5de85f6a8011917a4a86fb0
Diego Alonso Huamani Luque <dhuamanilu@unsa.edu.pe>
2024-10-23 15:41:47 -0500

    version final , se le dio formato al codigo , se actualizo el oncliklisetner  para que cada elemento abra su correspondiente fragment,clasutro con claustro y compania con compania

--- app/src/main/java/com/example/lab5_idnp/ClaustroFragment.java
+++ app/src/main/java/com/example/lab5_idnp/ClaustroFragment.java
@@ -9,16 +9,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class CatedralFragment extends Fragment {
+public class ClaustroFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_catedral, container, false);
+        View view = inflater.inflate(R.layout.fragment_claustro, container, false);
 
-        ImageView imageView = view.findViewById(R.id.catedralImage);
-        ImageUtils.applyCircularMask(imageView, R.drawable.catedral_image, getResources());
+        ImageView imageView = view.findViewById(R.id.claustroImage);
+        ImageUtils.applyCircularMask(imageView, R.drawable.claustro_image, getResources());
 
         return view;
     }
--- app/src/main/java/com/example/lab5_idnp/EdificacionesFragment.java
+++ app/src/main/java/com/example/lab5_idnp/EdificacionesFragment.java
@@ -17,18 +17,18 @@ public class EdificacionesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edificaciones, container, false);
-        // Aplicar la imagen circular a la Catedral
+
         ImageView catedralImage = view.findViewById(R.id.catedralImage);
         ImageUtils.applyCircularMask(catedralImage, R.drawable.catedral_image, getResources());
 
-        // Aplicar la imagen circular al Claustro
+
         ImageView claustroImage = view.findViewById(R.id.claustroImage);
         ImageUtils.applyCircularMask(claustroImage, R.drawable.claustro_image, getResources());
-        // Configurar onClick para la Catedral de Arequipa
+
         view.findViewById(R.id.catedralLayout).setOnClickListener(v -> openCatedralFragment());
 
-        // Configurar onClick para el Claustro de la Compañía
-        view.findViewById(R.id.claustroLayout).setOnClickListener(v -> openCatedralFragment());
+
+        view.findViewById(R.id.claustroLayout).setOnClickListener(v -> openClaustroFragment());
 
         return view;
     }
@@ -37,6 +37,13 @@ public class EdificacionesFragment extends Fragment {
         Fragment catedralFragment = new CatedralFragment();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_container, catedralFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+    private void openClaustroFragment() {
+        Fragment claustroFragment = new ClaustroFragment();
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, claustroFragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
